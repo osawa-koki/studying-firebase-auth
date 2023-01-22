8398cb0d6dcccd43a7cd347252be90f15452c036
osawa-koki <github@osawa.dev>
2023-01-22 17:12:34 +0900

    設定ファイル化。

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -3,19 +3,12 @@ import React, { useState } from "react";
 import { Button } from 'react-bootstrap';
 import Layout from "../components/Layout";
 
+// Firebase Authentication
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import { onAuthStateChanged } from 'firebase/auth';
-
-const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyDp0_U8B9ZM4mRequ0v5IjPpaLQHdNc8_8",
-  authDomain: "learning-firebaseauth.firebaseapp.com",
-  projectId: "learning-firebaseauth",
-  storageBucket: "learning-firebaseauth.appspot.com",
-  messagingSenderId: "627247538001",
-  appId: "1:627247538001:web:352cb8cbc290393053bd70",
-  measurementId: "G-F4SMG2381F",
-});
+import firebaseConfig from '../firebaseConfig';
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 const auth = firebaseApp.auth();
 onAuthStateChanged(auth, user => { console.log(user); });
 
