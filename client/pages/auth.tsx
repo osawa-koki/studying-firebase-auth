950b054dac5a65b6eefd9dedb38aa97af8d235bf
osawa-koki <github@osawa.dev>
2023-01-22 21:26:00 +0900

    認証情報の表示。

--- client/pages/auth.tsx
+++ client/pages/auth.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-import { Button } from 'react-bootstrap';
+import { Button, Table } from 'react-bootstrap';
 import Layout from "../components/Layout";
 
 // Firebase Authentication
@@ -15,10 +15,20 @@ const provider = new firebase.auth.GoogleAuthProvider();
 
 export default function HelloWorld() {
 
+  const [username, setUsername] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [token, setToken] = useState<string>('');
+
   const Auth = () => {
     // signInWithPopup or signInWithRedirect
     firebase.auth().signInWithPopup(provider).then(result => {
-      console.log(result);
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = result.credential as firebase.auth.OAuthCredential;
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      setUsername(user?.displayName || '');
+      setEmail(user?.email || '');
     }).catch(function(err) {
       console.error(err);
     });
@@ -31,6 +41,7 @@ export default function HelloWorld() {
       user.getIdToken().then(function(idToken) {
         // Send token to your server
         console.log(`idToken: ${idToken}`);
+        setToken(idToken);
         // fetch('/auth', {
         //   method: 'GET',
         //   headers: {
@@ -48,6 +59,28 @@ export default function HelloWorld() {
       <main>
         <div id="Auth">
           <Button variant="primary" onClick={Auth}>認証 🐸</Button>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>パラメタ</th>
+                <th>バリュー</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>username</th>
+                <td>{username}</td>
+              </tr>
+              <tr>
+                <th>email</th>
+                <td>{email}</td>
+              </tr>
+              <tr>
+                <th>token</th>
+                <td>{token}</td>
+              </tr>
+            </tbody>
+          </Table>
         </div>
       </main>
     </Layout>
